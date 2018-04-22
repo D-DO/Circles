0ea9c6d6505071a073f46e95ff6f708085acfb1c
D-DO <37467092+D-DO@users.noreply.github.com>
2018-04-22 21:48:17 +0600

    wiht and height of screen

--- Circles/app/src/main/java/com/example/odd/circles/CanvasView.java
+++ Circles/app/src/main/java/com/example/odd/circles/CanvasView.java
@@ -3,17 +3,33 @@ package com.example.odd.circles;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class CanvasView extends View {
+    public static int width;
+    public static int height;
     private GameManager gameManager;
 
 
     public CanvasView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        gameManager = new GameManager();
+        initWidthAndHeight(context);
+        gameManager = new GameManager(this, width, height);
+    }
+
+    private void initWidthAndHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        width = point.x;
+        height = point.y;
     }
 
 
